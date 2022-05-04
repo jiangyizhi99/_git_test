8fa37024725274128e3649a4d7b908437b7ae872
jiangyizhi99 <2504760223@qq.com>
2022-05-05 06:16:26 +0800

    create and fix

---
+++ _study_git/_study_git/_study_git.cpp
@@ -0,0 +1,35 @@
+﻿#include <iostream>
+#include "_test_data.h"
+
+int main()
+{
+    printf("printf:\n");
+    printf("%c\n", ch);
+    printf("%u\n", ui);
+    printf("%d\n", i);
+    printf("%ld\n", l);
+    printf("%1.2f\n", f);
+    printf("%lf\n", d);
+    for (int i = 0; i < sizeof chs; i++) {
+        printf("%c", chs[i]);
+    }
+    printf("\n");
+    printf("%ls\n", wchs);
+    printf("%s\n\n", chinese);
+
+    printf("snprintf:\n");
+    snprintf(ch_temp, sizeof ch, ch_p);
+    std::cout << ch_temp;
+    snprintf(ch_temp, sizeof ui, ui_p);
+    std::cout << ch_temp;
+    snprintf(ch_temp, sizeof i, i_p);
+    std::cout << ch_temp;
+    snprintf(ch_temp, sizeof l, l_p);
+    std::cout << ch_temp;
+    snprintf(ch_temp, sizeof f, f_p);
+    std::cout << ch_temp;
+    snprintf(ch_temp, sizeof d, d_p);
+    std::cout << ch_temp;
+
+    return 0;
+}
