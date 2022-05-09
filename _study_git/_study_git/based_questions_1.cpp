c199c23348272e583936243a762f3e1dd126359f
jiangyizhi99 <2504760223@qq.com>
2022-05-09 08:50:46 +0800

    feature:几个cpp文件的增加

---
+++ _study_git/_study_git/based_questions_1.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+
+class Student {
+public:
+    Student(const char* pName_) {
+        m_pName = (char*)malloc(strlen(pName_));
+        if (m_pName) {
+        memcpy(m_pName, pName_, strlen(pName_));
+        }
+    }
+    int GetAge() { return m_nAge; }
+    char* GetName() { return m_pName; }
+private:
+    int m_nAge;
+    char* m_pName;
+};
+
+void main_() {
+    Student s1("xiaowang");
+    Student s2 = s1;
+}
